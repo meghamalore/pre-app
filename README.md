5176ae071fd0dc3741f9d9db320d2ddca0f8c4c0
megha-ixl <megha.ixltech@gmail.com>
2026-02-02 14:36:59 +0530

    react setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# practical react Essentials
+This is my practice app for the course.
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mb-4'>Hello World</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
